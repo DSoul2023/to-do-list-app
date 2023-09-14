6152a2765656f3d9df9ed847fa963a41ab2ddb73
DSoul2023 <dantestaton2012@gmail.com>
2023-09-14 15:01:56 -0400

    Cross-out function conversion
    Converted crossOut function code from vanilla javascript to jQuery.

--- scripts.js
+++ scripts.js
@@ -7,19 +7,20 @@ let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
-
+if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+  
  //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+ function crossOut() {
+    li.toggleClass("strike");
+}
 
- 	li.addEventListener("dblclick",crossOut);
+li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+});
 
  //3(i). Adding the delete button "X": 
    let crossOutButton = document.createElement("crossOutButton");
@@ -35,3 +36,22 @@ li.append(inputValue);
    $('#list').sortable();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
