978ddbd29c2681b877a1d13a6ba60f8e11a9f5c0
DSoul2023 <dantestaton2012@gmail.com>
2023-09-15 05:29:35 -0400

    Restructured code
    added spacing and indentation for readability.

--- scripts.js
+++ scripts.js
@@ -1,45 +1,42 @@
-f
-function newItem(){
 
-//javascript
-//1. Adding a new item to the list of items: 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+function newItem() {
 
-if (inputValue === '') {
+  //javascript
+  //1. Adding a new item to the list of items: 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
     alert("You must write something!");
   } else {
     $('#list').append(li);
   }
-  
- //2. Crossing out an item from the list of items:
- function crossOut() {
+
+  //2. Crossing out an item from the list of items:
+  function crossOut() {
     li.toggleClass("strike");
-}
+  }
 
-li.on("dblclick", function crossOut() {
+  li.on("dblclick", function crossOut() {
     li.toggleClass("strike");
-});
+  });
 
- //3(i). Adding the delete button "X": 
- let crossOutButton = $('<crossOutButton></crossOutButton>');
- crossOutButton.append(document.createTextNode('X'));
- li.append(crossOutButton);
+   //3(i). Adding the delete button "X": 
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
 
- crossOutButton.on("click", deleteListItem);
+  crossOutButton.on("click", deleteListItem);
 
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
-        li.addClass("delete")
- 	}
- // 4. Reordering the items: 
-   $('#list').sortable();
+  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  function deleteListItem(){
+    li.addClass("delete")
+  }
 
+  // 4. Reordering the items:
+  $('#list').sortable();
 }
-
-
-
 
 
 
